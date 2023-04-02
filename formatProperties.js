6382a5d2df861920293619941670d2346b1ff584
MingHuang1220 <syuminghuang@gmail.com>
2023-04-02 20:17:06 +0800

    處理物件&陣列

--- formatProperties.js
+++ formatProperties.js
@@ -3,7 +3,7 @@ console.log(11)
 export class FormatProperties {
     constructor(data, type = 'lowerCamel') {
         // 英文字母-大寫
-        this.englishAlphabetUpperCase = [
+        this._englishAlphabetUpperCase = [
             'A',
             'B',
             'C',
@@ -32,7 +32,7 @@ export class FormatProperties {
             'Z',
         ]
         // 英文字母-小寫
-        this.englishAlphabetLowerCase = this.englishAlphabetUpperCase.map((item) => item.toLocaleLowerCase())
+        this._englishAlphabetLowerCase = this._englishAlphabetUpperCase.map((item) => item.toLocaleLowerCase())
 
         this.data = data
         this._typeList = [
@@ -44,25 +44,56 @@ export class FormatProperties {
 
         // 判斷輸入的類型是否正確
         if (!this._typeList.includes(this.type)) throw new Error('type only lowerCamel,UpperCamel,snake_case')
-        this.main(data, type)
+        const result = this.main(data)
+        return result.value
     }
 
     // 主要函式
-    main(data) {
+    main(data, property = '', depth = 0) {
         const dataType = typeof data // 資料類型
 
-        if (dataType !== 'object') throw new Error('data only object!')
+        if (depth === 0 && dataType !== 'object') throw new Error('data only object!')
 
         const isArray = Array.isArray(data) // 是不是陣列
+        // let realType = '' // 真正的類型
+        switch (dataType) {
+            case 'object':
+                // realType = isArray ? 'array' : 'object'
+                if (isArray) {
+                    // 陣列
+                    const nextDepth = depth + 1
+                    const newArray = []
+                    data.forEach((item, index) => {
+                        const result = this.main(item, String(index), nextDepth)
+                        const indexToNumber = Number(result.newPropertyName)
+                        newArray[indexToNumber] = result.value
+                    })
+                    const formatInfo = this.format(property, data)
 
-        if (isArray) {
-            // 如果是陣列
-        } else {
-            // 物件
-            Object.keys(data).forEach((property) => {
-                const value = data[property]
-                this.format(property, value)
-            })
+                    return {
+                        newPropertyName: formatInfo.newPropertyName,
+                        value: newArray,
+                    }
+                } else {
+                    // 物件
+                    const obj = {}
+                    const nextDepth = depth + 1
+                    Object.keys(data).forEach((propertyName) => {
+                        const value = data[propertyName]
+                        const result = this.main(value, propertyName, nextDepth)
+                        obj[result.newPropertyName] = result.value
+                    })
+                    const formatInfo = this.format(property, data)
+                    return {
+                        newPropertyName: formatInfo.newPropertyName,
+                        value: obj,
+                    }
+                }
+                break
+            default:
+                // realType = valueType
+                const formatInfo = this.format(property, data)
+                return formatInfo
         }
     }
 
@@ -72,40 +103,34 @@ export class FormatProperties {
      * @param {[*]} value 值
      */
     format(property = '', value) {
-        const strArray = property.split('') // 切割字串
-
         // 取得原本的命名方式
         const typeOriginal = this.getTypeOriginal(property)
-        console.log(typeOriginal)
         let newPropertyName = ''
-        if (typeOriginal === this.type) {
-            newPropertyName = property
-        } else if (typeOriginal === 'snake_case') {
-            if (this.type === 'lowerCamel') {
-                newPropertyName = this.snakeCaseToLowerCamel(property) // 蛇型轉小駝峰
-            } else if (this.type === 'UpperCamel') {
-                newPropertyName = this.snakeCaseToUpperCamel(property) // 蛇型轉大駝峰
-            }
-        } else if (typeOriginal === 'UpperCamel') {
-            if (this.type === 'snake_case') {
-                newPropertyName = this.upperLowerCamelToSnakeCase(property) // 大駝峰轉蛇型
-            } else if (this.type === 'lowerCamel') {
-                newPropertyName = this.upperCamelToLowerCamel(property) // 大駝峰轉小駝峰
-            }
-        } else if (typeOriginal === 'lowerCamel') {
-            if (this.type === 'snake_case') {
-                newPropertyName = this.upperLowerCamelToSnakeCase(property) // 小駝峰轉蛇型
-                // console.log('aa')
-            } else if (this.type === 'UpperCamel') {
-                newPropertyName = this.lowerCamelToUpperCamel(property) // 小駝峰轉大駝峰
-            }
-        } else {
-            newPropertyName = property
-        }
-        console.log(newPropertyName)
 
+        switch (typeOriginal) {
+            case this.type:
+                newPropertyName = property
+                break
+            case 'snake_case':
+                if (this.type === 'lowerCamel') newPropertyName = this.snakeCaseToLowerCamel(property) // 蛇型轉小駝峰
+                else if (this.type === 'UpperCamel') newPropertyName = this.snakeCaseToUpperCamel(property) // 蛇型轉大駝峰
+                break
+            case 'UpperCamel':
+                if (this.type === 'snake_case')
+                    newPropertyName = this.upperLowerCamelToSnakeCase(property) // 大駝峰轉蛇型
+                else if (this.type === 'lowerCamel') newPropertyName = this.upperCamelToLowerCamel(property) // 大駝峰轉小駝峰
+                break
+            case 'lowerCamel':
+                if (this.type === 'snake_case')
+                    newPropertyName = this.upperLowerCamelToSnakeCase(property) // 小駝峰轉蛇型
+                else if (this.type === 'UpperCamel') newPropertyName = this.lowerCamelToUpperCamel(property) // 小駝峰轉大駝峰
+                break
+            default:
+                newPropertyName = property
+        }
         return {
-            [newPropertyName]: value,
+            newPropertyName,
+            value,
         }
     }
 
@@ -114,10 +139,8 @@ export class FormatProperties {
     upperLowerCamelToSnakeCase(property = '') {
         const strArray = property.split('') // 切割字串
         // 抓大寫字母位置
-        const upperCaseIndex = []
-        strArray.forEach((item, index) => {
-            if (this.englishAlphabetUpperCase.includes(item)) upperCaseIndex.push(index)
-        })
+        const upperCaseIndex = this.getUpperEnglishAlphabet(strArray) // 取得大寫字母位置
+
         // 大寫字母前面+底線
         let newPropertyName = ''
         strArray.forEach((item, index) => {
@@ -147,10 +170,7 @@ export class FormatProperties {
     upperCamelToLowerCamel(property = '') {
         const strArray = property.split('') // 切割字串
         // 抓大寫字母位置
-        const upperCaseIndex = []
-        strArray.forEach((item, index) => {
-            if (this.englishAlphabetUpperCase.includes(item)) upperCaseIndex.push(index)
-        })
+        const upperCaseIndex = this.getUpperEnglishAlphabet(strArray) // 取得大寫字母位置
 
         let newPropertyName = ''
         strArray.forEach((item, index) => {
@@ -172,10 +192,7 @@ export class FormatProperties {
     lowerCamelToUpperCamel(property = '') {
         const strArray = property.split('') // 切割字串
         // 抓大寫字母位置
-        const upperCaseIndex = []
-        strArray.forEach((item, index) => {
-            if (this.englishAlphabetUpperCase.includes(item)) upperCaseIndex.push(index)
-        })
+        const upperCaseIndex = this.getUpperEnglishAlphabet(strArray) // 取得大寫字母位置
         let newPropertyName = ''
         strArray.forEach((item, index) => {
             if (index === 0) {
@@ -208,6 +225,19 @@ export class FormatProperties {
         const itemStrLowerCaseArray = itemStrArray.join('')
         return firstEnglishAlphabet + itemStrLowerCaseArray
     }
+    /**
+     * 取得大寫字母位置
+     * @param {[Array]} strArray 切割後的字串陣列
+     * @return {[Array]} 大寫字母的索引
+     */
+    getUpperEnglishAlphabet(strArray = []) {
+        // 抓大寫字母位置
+        const upperCaseIndex = []
+        strArray.forEach((item, index) => {
+            if (this._englishAlphabetUpperCase.includes(item)) upperCaseIndex.push(index)
+        })
+        return upperCaseIndex
+    }
 
     // 取得原本的命名方式
     getTypeOriginal(property = '', index = 0) {
@@ -219,8 +249,8 @@ export class FormatProperties {
             const strArray = property.split('') // 切割字串
             // 字是大寫還是小寫
             const englishAlphabet = strArray[index]
-            const isUpperCase = this.englishAlphabetUpperCase.includes(englishAlphabet)
-            const isLowerCase = this.englishAlphabetLowerCase.includes(englishAlphabet)
+            const isUpperCase = this._englishAlphabetUpperCase.includes(englishAlphabet)
+            const isLowerCase = this._englishAlphabetLowerCase.includes(englishAlphabet)
 
             if (isUpperCase) typeOriginal = 'UpperCamel' // 大駝峰
             else if (isLowerCase) typeOriginal = 'lowerCamel' // 小駝峰
@@ -236,19 +266,5 @@ export class FormatProperties {
             }
         }
         return typeOriginal
-    }
-}
-
-export const formatProperties = (data) => {
-    const dataType = typeof data // 資料類型
-
-    if (dataType !== 'object') throw new Error('only object!')
-
-    const isArray = Array.isArray(data) // 是不是陣列
-
-    if (isArray) {
-        // 如果是陣列
-    } else {
-        // 物件
     }
 }
